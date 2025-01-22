699f391cfb26ba3e20e88888bf7152e4e4e8bb4f
dani <danii0110@naver.com>
2025-01-22 13:37:00 +0900

    ch01 예제

---
+++ JavaBasic/ch01/Hello.java
@@ -0,0 +1,12 @@
+package JavaBasic.ch01;
+
+/**
+ * @author dani
+ */
+public class Hello {
+    // 프로그램 실행 진입점
+    public static void main(String[] args) {
+        // 콘솔에 출력하는 실행문
+        System.out.println("Hello, Java");
+    }
+}
